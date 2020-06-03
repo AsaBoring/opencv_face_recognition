267bb50630d1f4fad80f00ff46519c4232f117e6
AsaBoring <47557794+AsaBoring@users.noreply.github.com>
2020-06-03 16:04:49 +0800

    添加从本地识别模型读取数据识别的功能

--- main.cpp
+++ main.cpp
@@ -105,6 +105,7 @@ void asa_face_recg(string haar_path, string csv_path, int recg_label, int device
     /*training model*/
     Ptr<EigenFaceRecognizer> recg_model = EigenFaceRecognizer::create();
     recg_model->train(images,labels);
+    recg_model->save("./eigenface.yml");
 
     /*load haar model*/
     CascadeClassifier haar_cascade;
@@ -147,6 +148,52 @@ void asa_face_recg(string haar_path, string csv_path, int recg_label, int device
     }
 }
 
+void asa_face_recg_by_load_yml(string haar_path , string yml_path, int recg_label, int device_id)
+{
+    /*load yml file*/
+    Ptr<EigenFaceRecognizer> recg_model = Algorithm::load<EigenFaceRecognizer>(yml_path.c_str());
+
+    /*load haar model*/
+    CascadeClassifier haar_cascade;
+    haar_cascade.load(haar_path);
+
+    /*start up capture*/
+    VideoCapture cap(device_id);
+    if(!cap.isOpened())
+    {
+        cout<<"open video capture failed"<<endl;
+        return;
+    }
+    Mat frame;
+    int width = 96;
+    int height = 112;
+    while (cap.read(frame)) {
+        /*旋转成像*/
+        flip(frame,frame,1);
+        Mat gray;
+        cvtColor(frame,gray,COLOR_BGR2GRAY);
+        vector<Rect> faces;
+        haar_cascade.detectMultiScale(gray,faces);
+        for(size_t i = 0 ; i < faces.size() ; ++i)
+        {
+            Rect face_i = faces[i];
+            Mat face = gray(face_i);
+            Mat predict_face;
+            resize(face,predict_face,Size(width,height),1.0,1.0,INTER_CUBIC);
+            int face_label = recg_model->predict(predict_face);
+            rectangle(frame,faces[i],Scalar(0,0,255),2,8,0);
+            putText(frame,face_label==recg_label?"This is Asa , hello":"who are tou",face_i.tl(),FONT_HERSHEY_PLAIN,1.0,Scalar(255,0,0));
+        }
+
+        imshow("recg_frame",frame);
+        char c = (char)waitKey(10);
+        if(c == 27)
+        {
+            break;
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
@@ -155,7 +202,8 @@ int main(int argc, char *argv[])
      * 此项目运行需要添加opencv+contrib的lib
      * 先执行asa_clooect_face_data收集面部数据
      * 在执行asa_produce_csv生成csv文件以便face recg模型使用
-     * 最后执行asa_face_recg查看识别效果如何
+     * 最后执行asa_face_recg保存模型文件查看识别效果如何
+     * 另外asa_face_recg_by_load_yml加载b本地的yml文件进行识别
     */
 
     /*面部识别的图像标记，可自定义*/
@@ -167,7 +215,8 @@ int main(int argc, char *argv[])
     string csv_path = "C:/Users/houxia2x/Desktop/Asa/opencv/project/PIC_DATA/image.csv";
     /*opencv的haar model的文件路径*/
     string haar_path = "C:/Users/houxia2x/Desktop/Asa/opencv/opencv/build/etc/haarcascades/haarcascade_frontalface_alt_tree.xml";
-
+    /*yml文件路径*/
+    string yml_path = "./eigenfaces.yml";
     //    /*收集人脸图片
     //     * 1.在执行此函数时请头部保持不动，在眉眼保持不变的情况下做尽可能多的表情
     //     * 2.此函数每10帧进行一次图片保存，大概保存15~20张图片即可，请自行按ESC键退出
@@ -179,11 +228,9 @@ int main(int argc, char *argv[])
     //    asa_produce_csv(csv_path,dir_path);
 
     /*上述两步完成后即可执行此函数进行是被效果验证*/
-    asa_face_recg(haar_path,csv_path,recg_label);
+    //    asa_face_recg(haar_path,csv_path,recg_label);
 
+    /*加载本地识别模型进行人脸识别*/
+    asa_face_recg_by_load_yml(haar_path,yml_path,recg_label);
     return a.exec();
 }
-
-
-
-
